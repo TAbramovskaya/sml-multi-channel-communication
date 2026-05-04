d40680db8d2e507d47e97fc3ccc746c92abea148
TAbramovskaya <tanya.abramovskaya@gmail.com>
2026-05-05 01:21:20 +0300

    Add pairwise message comparison and summary

--- compare_messages.py
+++ compare_messages.py
@@ -1,34 +1,59 @@
+import pandas as pd
+
+
 def copypaste_score(text, other):
     words_test = text.split(' ')
     words_other = other.split(' ')
 
-    i = 0
-    j = 0
-    l = 0
     longest = 0
 
-    while i < len(words_test):
-        wt = words_test[i]
-        while j < len(words_other):
-            wo = words_other[j]
-            while wt == wo:
-                l += 1
-                if i + l < len(words_test) and j + l < len(words_other):
-                    wt = words_test[i + l]
-                    wo = words_other[j + l]
-                else:
-                    break
-            if l > longest:
-                longest = max(longest, l)
-                print(f'test: {words_test[i:i + l]}')
-                print(f'other: {words_other[j:j + l]}')
+    for i in range(len(words_test)):
+        for j in range(len(words_other)):
             l = 0
-            j += 1
-        i += 1
+            while i + l < len(words_test) and j + l < len(words_other) and words_test[i + l] == words_other[j + l]:
+                l += 1
+            if l > longest:
+                longest = l
 
-    return longest, len(words_test), len(words_other)
+    return longest
 
 
+def pairwise_similarity_table(text_messages):
+    daily_comparisons = []
+
+    for day, group in text_messages.groupby('day'):
+        pairs = (group
+                  .merge(group, how='cross', suffixes=('_1', '_2'))
+                  .query('source_1 != source_2')
+                  .query('id_1 < id_2')
+                  )
+        if pairs.empty:
+            continue
+
+        pairs['copypaste_score'] = [copypaste_score(a, b) for a, b in zip(pairs['content_1'], pairs['content_2'])]
+        pairs['is_similar'] = pairs['copypaste_score'] >= 15
+        daily_comparisons.append(pairs)
+
+    pairwise_messages = pd.concat(daily_comparisons, ignore_index=True)
+    pairwise_messages['day'] = pairwise_messages['day_1']
+    pairwise_messages['id'] = pairwise_messages.index
+    pairwise_messages = pairwise_messages[['id', 'day', 'copypaste_score', 'is_similar', 'source_1', 'source_2', 'source_id_1', 'source_id_2', 'content_1' , 'content_2']]
+
+    pairwise_messages.to_csv("csv/pairwise_messages.csv", index=False)
+
+    return pairwise_messages
 
 
+def similar_count(pairwise_messages):
+    similarity_summary = (
+        pairwise_messages[pairwise_messages['is_similar']]
+        .assign(
+            source_a=lambda df: df[['source_1', 'source_2']].min(axis=1),
+            source_b=lambda df: df[['source_1', 'source_2']].max(axis=1),
+        )
+        .groupby(['day', 'source_a', 'source_b'])
+        .size()
+        .reset_index(name='similar_count')
+    )
 
+    return similarity_summary
--- main.py
+++ main.py
@@ -1,14 +1,42 @@
 import build_df
 import pandas as pd
+from compare_messages import *
+
+pd.set_option('display.max_columns', None)
 
 
 if __name__ == "__main__":
+    # ---
+    # Option 1: Update data from raw JSON files and current emails and export data to CSV files:
+    # messages, *_ = build_df.from_raw_data()
 
-    # To update data from raw JSON files and current emails:
-    # messages = build_df.from_raw_data()
+    #Option 2: Load data from ready-made CSV files:
+    messages, *_ = build_df.from_csv()
+    # ---
 
-    # To load data from ready-made CSV files:
-    messages = build_df.from_csv()
     messages['day'] = pd.to_datetime(messages['date']).dt.floor('D')
 
+    # Remove empty text messages and messages typed as 'other'
+    messages = messages[
+                  ~(
+                    ((messages['content'].isna()) & (messages['type'] == 'text'))
+                    | (messages['type'] == 'other')
+                    )
+                ]
+
+    text_messages = messages[messages['type'] == 'text'].copy()
+    text_messages = text_messages[['id', 'source_id', 'source', 'day', 'content']]
+
+    # ---
+    # Option 1: Update pairwise message comparison from current DataFrame
+    # pairwise_messages = pairwise_similarity_table(text_messages)
+
+    # Option 2: Load data from ready-made CSV files:
+    pairwise_messages = pd.read_csv("csv/pairwise_messages.csv")
+    pairwise_messages["day"] = pd.to_datetime(pairwise_messages["day"]).dt.floor('D')
+    #---
+
+
+    similarity_summary = similar_count(pairwise_messages)
+    similarity_summary.to_csv("csv/similarity_summary.csv", index=False)
 
