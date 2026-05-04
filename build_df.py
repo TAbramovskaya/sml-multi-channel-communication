bf13df4a28e1861231b3b2aa81c17916b615cb1a
TAbramovskaya <tanya.abramovskaya@gmail.com>
2026-05-05 01:18:45 +0300

    Adjust column selection and order in CSV export

--- build_df.py
+++ build_df.py
@@ -15,29 +15,33 @@ def from_raw_data():
 
     intensives = build_dataframe_json(intensives_file, intensives_label)
     events = build_dataframe_json(events_file, events_label)
-    gmail = build_dataframe_gmail(mailbox_service, mailbox_label)
+    mailbox = build_dataframe_gmail(mailbox_service, mailbox_label)
 
-    intensives.to_csv("csv/intensives.csv")
-    events.to_csv("csv/events.csv")
-    gmail.to_csv("csv/gmail.csv")
+    intensives.to_csv("csv/intensives.csv", index=False)
+    events.to_csv("csv/events.csv", index=False)
+    mailbox.to_csv("csv/gmail.csv", index=False)
 
-    messages = pd.concat([intensives, events, gmail], axis=0, ignore_index=True)
-    # messages = messages.drop(columns=["content_structured"])
-    messages = messages.sort_values("date")
-    messages.to_csv("csv/messages.csv")
+    messages = pd.concat([intensives, events, mailbox], axis=0, ignore_index=True)
+    messages = messages.sort_values("date").reset_index(drop=True)
+    messages["id"] = messages.index
 
-    return messages
+    messages = messages[["id", "source_id", "source", "type", "date", "content", "content_structured"]]
+
+    messages.to_csv("csv/messages.csv", index=False)
+
+    return messages, intensives, events, mailbox
 
 
 def from_csv():
     intensives = pd.read_csv("csv/intensives.csv")
     events = pd.read_csv("csv/events.csv")
-    gmail = pd.read_csv("csv/gmail.csv")
-    messages = pd.read_csv("csv/messages.csv")
+    mailbox = pd.read_csv("csv/gmail.csv")
 
     intensives["date"] = pd.to_datetime(intensives["date"])
     events["date"] = pd.to_datetime(events["date"])
-    gmail["date"] = pd.to_datetime(gmail["date"])
+    mailbox["date"] = pd.to_datetime(mailbox["date"])
+
+    messages = pd.read_csv("csv/messages.csv")
     messages["date"] = pd.to_datetime(messages["date"])
 
-    return messages
+    return messages, intensives, events, mailbox
