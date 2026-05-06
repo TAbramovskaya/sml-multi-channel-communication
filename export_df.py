89a49a17b3325624112f709701f7c3bde7e9bc6f
TAbramovskaya <tanya.abramovskaya@gmail.com>
2026-05-06 03:26:22 +0300

    Minor refactoring

--- export_df.py
+++ export_df.py
@@ -7,12 +7,12 @@ def to_csv(df, name, prefix='csv/'):
     df.to_csv(path, index=False)
 
 
-def to_many_csv(*dfs, names=None):
+def to_many_csv(*dfs, names=None, prefix='csv/'):
     if not names or len(dfs) != len(names):
         print('Export failed: names argument does not match dfs argument')
         return
     for df, name in zip(dfs, names):
-        path = 'csv/' + name + '.csv'
+        path = prefix + name + '.csv'
         df.to_csv(path, index=False)
 
 
@@ -29,10 +29,7 @@ def to_csv_cropped(dfs_dict, day_start="2026-01-01", day_end="2026-04-30", no_co
         col = 'day' if 'day' in new_df.columns else 'date'
 
         start = pd.to_datetime(day_start, utc=True)
-        end = pd.to_datetime(day_end, utc=True)
-
-        if col == 'date':
-            end = end + pd.Timedelta(days=1)
+        end = pd.to_datetime(day_end, utc=True) + pd.Timedelta(days=1)
 
         new_df = new_df[(new_df[col] >= start) & (new_df[col] < end)]
 
--- main.py
+++ main.py
@@ -8,13 +8,14 @@ pd.set_option('display.max_columns', None)
 
 if __name__ == "__main__":
 
-    result = build_df.load(from_csv=["messages", "intensives", "events", "mailbox",
-                                     "text_messages", "msg_pairwise_similarity", "day_source_msg_stats"])
+    result = build_df.load()
+    # To load from CSV use from_csv argument. Full list of file names is
+    # ["messages", "intensives", "events", "mailbox", "text_messages", "msg_pairwise_similarity", "day_source_msg_stats"]
 
-    # result = build_df.load()
-    #
-    # for name, df in result.items():
-    #     if df is not None:
-    #         export_df.to_csv(df, name)
+    for name, df in result.items():
+        if df is not None:
+            export_df.to_csv(df, name)
+
+    export_df.to_jsonl(result['text_messages'], sources=['events'])
 
     cropped = export_df.to_csv_cropped(result)
