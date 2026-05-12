f9bfc404c0be96cf39aabef399cee9f0314d10f0
TAbramovskaya <tanya.abramovskaya@gmail.com>
2026-05-12 14:04:03 +0300

    Minor refactoring

--- src/ai_analysis/postprocess_config.py
+++ src/ai_analysis/postprocess_config.py
@@ -1,36 +1,3 @@
-aliases = {
-    'admin' : 'Simulative',
-    'Павел Беляев': 'Pavel B.',
-    'Pavel Belyaev': 'Pavel B.',
-    'Наталья Рожкова': 'Natalia R.',
-    'Георгий Семенов': 'Georgii S.',
-    'Георгий Семёнов': 'Georgii S.',
-    'Georgiy Semenov': 'Georgii S.',
-    'Андрон': 'Andron A.',
-    'Andron': 'Andron A.',
-    'Andron Aleksanyan': 'Andron A.',
-    'Кристина Желтова':'Kristina Zh.',
-    'Роман Филонов': 'Roman F.',
-    'Мария Жарова': 'Mariia Zh.',
-    'Анастасия Кузнецова': 'Anastasiia K.',
-    'Евгения Анкудинова': 'Evgeniia A.',
-    'Вячеслав Потапов': 'Viacheslav P.',
-    'Настя Кузнецова': 'Anastasiia K.',
-    'Андрон Алексанян': 'Andron A.',
-    'Марина Ермак': 'Marina E.',
-    'Marina Ermak': 'Marina E.',
-    'Евгений Буторин': 'Evgenii B.',
-    'Evgeniy Butorin': 'Evgenii B.',
-    'Александр Грудинин': 'Aleksandr G.',
-    'Аслан Байрамкулов': 'Aslan B.',
-    'Илья Ковалёв': 'Ilia K.',
-    'Денис Иванов': 'Denis I.',
-    'Александр Дарьин': 'Aleksandr D.',
-    'Татьяна Смоленкова': 'Tatiana S.',
-    'Команда Simulative': 'Simulative',
-    'команда Симулейтив': 'Simulative'
-}
-
 tags_rus_eng = {
     "tag_intent": {
         "конверсия в обучение": "conversion to course enrollment",
@@ -63,3 +30,36 @@ transformation_labels = [
         "strong rewrite",
         "heavy reconstruction"
 ]
+
+aliases = {
+    'admin' : 'Simulative',
+    'Павел Беляев': 'Pavel B.',
+    'Pavel Belyaev': 'Pavel B.',
+    'Наталья Рожкова': 'Natalia R.',
+    'Георгий Семенов': 'Georgii S.',
+    'Георгий Семёнов': 'Georgii S.',
+    'Georgiy Semenov': 'Georgii S.',
+    'Андрон': 'Andron A.',
+    'Andron': 'Andron A.',
+    'Andron Aleksanyan': 'Andron A.',
+    'Кристина Желтова':'Kristina Zh.',
+    'Роман Филонов': 'Roman F.',
+    'Мария Жарова': 'Mariia Zh.',
+    'Анастасия Кузнецова': 'Anastasiia K.',
+    'Евгения Анкудинова': 'Evgeniia A.',
+    'Вячеслав Потапов': 'Viacheslav P.',
+    'Настя Кузнецова': 'Anastasiia K.',
+    'Андрон Алексанян': 'Andron A.',
+    'Марина Ермак': 'Marina E.',
+    'Marina Ermak': 'Marina E.',
+    'Евгений Буторин': 'Evgenii B.',
+    'Evgeniy Butorin': 'Evgenii B.',
+    'Александр Грудинин': 'Aleksandr G.',
+    'Аслан Байрамкулов': 'Aslan B.',
+    'Илья Ковалёв': 'Ilia K.',
+    'Денис Иванов': 'Denis I.',
+    'Александр Дарьин': 'Aleksandr D.',
+    'Татьяна Смоленкова': 'Tatiana S.',
+    'Команда Simulative': 'Simulative',
+    'команда Симулейтив': 'Simulative'
+}
